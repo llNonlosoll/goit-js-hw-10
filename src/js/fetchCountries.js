36d755b93ea243e369e8b9bf41854169bede060c
llNonlosoll <wishmasterar@gmail.com>
2023-04-18 22:10:01 +0200

    404 catch method added

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -8,6 +8,12 @@ function fetchCountries(name) {
       // console.log(Error(result.status));
       // console.log(Error(result.statusText));
       // console.log(result.status);
+
+      // Спосіб вивести Notify.failure при помилці 404
+      // if (result.status === 404) {
+      //   Notify.failure('Oops, there is no country with that name');
+      //   throw new Error(result.status);
+      // }
       throw new Error(result.status);
     }
     return result.json();
